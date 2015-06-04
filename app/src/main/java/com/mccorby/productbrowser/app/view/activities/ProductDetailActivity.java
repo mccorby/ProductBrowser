1b030da85f04722a9d5d2dda0880b7535c9bcde8
Jose C <joseci@goldengekko.com>
2015-06-04 16:49:54 +0200

    Detail skeleton

---
+++ app/src/main/java/com/mccorby/productbrowser/app/view/activities/ProductDetailActivity.java
@@ -0,0 +1,15 @@
+package com.mccorby.productbrowser.app.view.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import com.mccorby.productbrowser.R;
+
+public class ProductDetailActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_detail);
+    }
+}
